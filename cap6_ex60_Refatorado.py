e688051fa4938f3d24fddcad9631350ff6dc1cf6
Francisco de Assis P A Santos <35563930+Chikuss2@users.noreply.github.com>
2019-12-10 03:09:11 -0300

    Add files via upload

---
+++ cap6_ex60_Refatorado.py
@@ -0,0 +1,31 @@
+"""
+Exercicio 60
+"""
+
+
+while True:
+    try:
+        lista = []
+        while True:
+            try:
+                num = int(input("Numero: "))
+                if num == 0:
+                    raise ValueError
+                else:
+                    lista.append(num)
+            except ValueError:
+                break
+        if len(lista) > 0:
+            print(f'Soma dos numeros digitados = {sum(lista)}')
+            print(f'Quantidade de numeros digitados = {len(lista)}')
+            print(f'A media dos numeros digitados é = {(sum(lista)/len(lista)):.2f}')
+            print(f'Maior numero digitado foi o {max(lista)}')
+            print(f'Menor numero digitado foi o {min(lista)}')
+            print(f'Media dos numeros pares é = '
+                  f'{sum([x for x in lista if x % 2 == 0])/len([x for x in lista if x % 2 ==0]):.2f}')
+            break
+        else:
+            raise ValueError
+    except ValueError:
+        print('Finalizando o Programa')
+        break
