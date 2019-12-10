69d9b5ee05509ce9335823521a5e1c29b08a3283
Francisco de Assis P A Santos <35563930+Chikuss2@users.noreply.github.com>
2019-12-10 03:12:32 -0300

    Update cap6_ex60_Refatorado.py

--- cap6_ex60_Refatorado.py
+++ cap6_ex60_Refatorado.py
@@ -1,5 +1,12 @@
 """
-Exercicio 60
+Exercicio 60 - Faça um programa que leia vários números, calcule e mostre:
+a) A soma dos números digitados
+b) A quantidade de números digitados
+c) A média dos números digitados
+d) O maior número digitado
+e) O menor número digitado
+f) A média dos números pares
+Finalize a entrada de dados caso o usuário informe o valor 0.
 """
 
 
